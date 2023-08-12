0dc4c1f94b149837534a2e5659c7af7edd197301
Rafael Rizzo <142092363+rafaelRizzoPVOX@users.noreply.github.com>
2023-08-12 09:39:34 -0300

    limpando e padronizando o projeto

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="pt-br">
+            <meta name="robots" content="noindex,nofollow" />
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
